1eab52d221dc000918abe3fa8fbda7469c61345d
Riya rai <118008485+riya053@users.noreply.github.com>
2023-07-13 18:50:13 +0530

    Create mathfunction.java

---
+++ mathfunction.java
@@ -0,0 +1,16 @@
+package myproject;
+
+public class Mathfunctions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int p=Math.max(5, 6);
+		System.out.println(p);
+		int q=Math.min(67, 37);
+		System.out.println(q);
+		
+		System.out.println(Math.random());
+
+	}
+
+}
