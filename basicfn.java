5bea267d6057e0c349a9b662046a068ce65724c4
Riya rai <118008485+riya053@users.noreply.github.com>
2023-07-13 18:48:35 +0530

    Create basicfn.java

---
+++ basicfn.java
@@ -0,0 +1,27 @@
+package myproject;
+
+public class Basicfn {
+	public static void printfn()
+	{
+		System.out.println("Hello simran");
+	}
+	public static void printname(String name)
+	{
+		System.out.println(name);
+	}
+    public static void printsum(int a,int b)
+    {
+    	int sum=a+b;
+    	System.out.println(sum);
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printfn();
+		printfn();
+		printfn();
+		printname("kesar");
+		printsum(4,6);
+
+	}
+
+}
