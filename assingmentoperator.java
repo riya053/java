283326a5f99f40d3964d5b85b3c6a454f6d5ac79
Riya rai <118008485+riya053@users.noreply.github.com>
2023-07-13 18:50:49 +0530

    Create assingmentoperator.java

---
+++ assingmentoperator.java
@@ -0,0 +1,21 @@
+package myproject;
+
+public class assingmentoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      double a=7;
+      double b=2;
+     double sum=a+b;//sum
+      System.out.println(sum);
+      double diff=a-b;//diff
+      System.out.println(diff);
+      double mul=a*b;//mul
+      System.out.println(mul);
+      double div=a/b;//division
+      System.out.println(div);
+      double rem=a%b;//remainder
+      System.out.println(rem);
+	}
+
+}
