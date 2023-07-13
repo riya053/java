ac0d489415695af38529d8d5b36e5371dbf10b05
Riya rai <118008485+riya053@users.noreply.github.com>
2023-07-13 18:47:41 +0530

    Create butterflypattern.java

---
+++ butterflypattern.java
@@ -0,0 +1,47 @@
+package myproject;
+import java.util.Scanner;
+
+public class Butterflypattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int m=sc.nextInt();
+		for(int i=1;i<=m;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			
+			int spaces=2*(m-i);
+			for(int j=1;j<=spaces;j++) {
+				System.out.print(" ");
+				
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+		for(int i=m;i>=1;i--)
+		{
+		  for(int j=1;j<=i;j++)
+		  {
+			  System.out.print("*");
+		  }
+		  int spaces=2*(m-i);
+		  for(int j=1;j<=spaces;j++)
+		  {
+			  System.out.print(" ");
+		  }
+		  for(int j=1;j<=i;j++)
+		  {
+			  System.out.print("*");
+			  }
+		  System.out.println();
+		  }
+	}
+
+}
